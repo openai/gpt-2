7eb02f8410d4afec2a162acb1a93b1759102e9da
Max Woolf <max@minimaxir.com>
2019-02-14 11:00:28 -0800

    Remove trailing forward-flash

--- download_model.sh
+++ download_model.sh
@@ -4,5 +4,5 @@ mkdir models/$model
 
 # TODO: gsutil rsync -r gs://gpt-2/models/ models/
 for filename in checkpoint encoder.json hparams.json model.ckpt.data-00000-of-00001 model.ckpt.index model.ckpt.meta vocab.bpe; do
-  gsutil cp gs://gpt-2/models/$model/$filename models/$model/
+  gsutil cp gs://gpt-2/models/$model/$filename models/$model
 done
