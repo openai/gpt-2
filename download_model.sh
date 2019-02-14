69354b7661cc3f4291a9ee449e36e7ed147e8292
Jeff Wu <wuthefwasthat@gmail.com>
2019-02-14 11:36:43 -0800

    mkdir -p

--- download_model.sh
+++ download_model.sh
@@ -1,6 +1,6 @@
 model=$1
 
-mkdir models/$model
+mkdir -p models/$model
 
 # TODO: gsutil rsync -r gs://gpt-2/models/ models/
 for filename in checkpoint encoder.json hparams.json model.ckpt.data-00000-of-00001 model.ckpt.index model.ckpt.meta vocab.bpe; do
