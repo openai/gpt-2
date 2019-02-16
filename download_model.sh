f9bd5f096644fd97e423919645f4cb02bbf48884
Mathieu Rene <mathieu@zia.ai>
2019-02-16 10:32:30 -0500

    Fetch model using curl, add shebang to download_files.sh and mark it executable

--- download_model.sh
+++ download_model.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 if [ "$#" -ne 1 ]; then
     echo "You must enter the model name as a parameter, e.g.: sh download_model.sh 117M"
     exit 1
@@ -9,5 +11,7 @@ mkdir -p models/$model
 
 # TODO: gsutil rsync -r gs://gpt-2/models/ models/
 for filename in checkpoint encoder.json hparams.json model.ckpt.data-00000-of-00001 model.ckpt.index model.ckpt.meta vocab.bpe; do
-  gsutil cp gs://gpt-2/models/$model/$filename models/$model
+  fetch=$model/$filename
+  echo "Fetching $fetch"
+  curl --output models/$fetch https://storage.googleapis.com/gpt-2/models/$fetch
 done
