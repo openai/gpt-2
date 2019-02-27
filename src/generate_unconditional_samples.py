1d8a942acd89aa5e0a264b08444397248ff45806
ArmaanBhullar <jhaniria@gmail.com>
2019-02-27 10:54:37 +0530

    Fix help flag by adding function docstring

--- src/generate_unconditional_samples.py
+++ src/generate_unconditional_samples.py
@@ -17,6 +17,26 @@ def sample_model(
     temperature=1,
     top_k=0,
 ):
+    """
+    Run the sample_model
+    :model_name=117M : String, which model to use
+    :seed=None : Integer seed for random number generators, fix seed to
+     reproduce results
+    :nsamples=0 : Number of samples to return, if 0, continues to
+     generate samples indefinately.
+    :batch_size=1 : Number of batches, model runs nsamples//batch_size
+     times, each batch run is independent of previous run.
+    :length=None : Number of tokens in generated text, if None (default), is
+     determined by model hyperparameters
+    :temperature=1 : Float value controlling randomness in boltzmann
+     distribution. Lower temperature results in less random completions. As the
+     temperature approaches zero, the model will become deterministic and
+     repetitive. Higher temperature results in more random completions.
+    :top_k=0 : Integer value controlling diversity. 1 means only 1 word is
+     considered for each step (token), resulting in deterministic completions,
+     while 40 means 40 words are considered at each step. 0 (default) is a
+     special setting meaning no restrictions. 40 generally is a good value.
+    """
     enc = encoder.get_encoder(model_name)
     hparams = model.default_hparams()
     with open(os.path.join('models', model_name, 'hparams.json')) as f:
--- src/interactive_conditional_samples.py
+++ src/interactive_conditional_samples.py
@@ -10,24 +10,29 @@ def interact_model(
     model_name='117M',
     seed=None,
     nsamples=1,
-    batch_size=None,
+    batch_size=1,
     length=None,
     temperature=1,
     top_k=0,
 ):
     """
     Interactively run the model
-    :model_name=117M : which model to use (default 117M)
-    :seed=None : Seed for random number generators, fix seed to reproduce
+    :model_name=117M : String, which model to use
+    :seed=None : Integer seed for random number generators, fix seed to reproduce
      results
-    :nsamples=1 : number of samples to return
-    :batch_size=None : Number of batches, model runs nsamples//batch_size
+    :nsamples=1 : Number of samples to return
+    :batch_size=1 : Number of batches, model runs nsamples//batch_size
      times, each batch run is independent of previous run.
-    :length=None : Length of text to be returned, inclusive of punctuations
-     etc.
-    :temperature=1 : Controls degree of surprise in final output
-    :top_k=0 : Number of logits to be sampled, top_k=1 gives deterministic
-     output
+    :length=None : Number of tokens in generated text, if None (default), is
+     determined by model hyperparameters
+    :temperature=1 : Float value controlling randomness in boltzmann
+     distribution. Lower temperature results in less random completions. As the
+     temperature approaches zero, the model will become deterministic and
+     repetitive. Higher temperature results in more random completions.
+    :top_k=0 : Integer value controlling diversity. 1 means only 1 word is
+     considered for each step (token), resulting in deterministic completions,
+     while 40 means 40 words are considered at each step. 0 (default) is a
+     special setting meaning no restrictions. 40 generally is a good value.
     """
     if batch_size is None:
         batch_size = 1
