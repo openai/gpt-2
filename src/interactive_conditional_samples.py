ed49f037786773fc84fbc573e6a94e3bc6fe1a80
Armaan Bhullar <ArmaanBhullar@users.noreply.github.com>
2019-02-27 12:31:38 +0530

    Add documentation for help flags (#81)
    add description for flags

--- src/interactive_conditional_samples.py
+++ src/interactive_conditional_samples.py
@@ -12,11 +12,30 @@ def interact_model(
     model_name='117M',
     seed=None,
     nsamples=1,
-    batch_size=None,
+    batch_size=1,
     length=None,
     temperature=1,
     top_k=0,
 ):
+    """
+    Interactively run the model
+    :model_name=117M : String, which model to use
+    :seed=None : Integer seed for random number generators, fix seed to reproduce
+     results
+    :nsamples=1 : Number of samples to return
+    :batch_size=1 : Number of batches, model runs nsamples//batch_size
+     times, each batch run is independent of previous run.
+    :length=None : Number of tokens in generated text, if None (default), is
+     determined by model hyperparameters
+    :temperature=1 : Float value controlling randomness in boltzmann
+     distribution. Lower temperature results in less random completions. As the
+     temperature approaches zero, the model will become deterministic and
+     repetitive. Higher temperature results in more random completions.
+    :top_k=0 : Integer value controlling diversity. 1 means only 1 word is
+     considered for each step (token), resulting in deterministic completions,
+     while 40 means 40 words are considered at each step. 0 (default) is a
+     special setting meaning no restrictions. 40 generally is a good value.
+    """
     if batch_size is None:
         batch_size = 1
     assert nsamples % batch_size == 0
