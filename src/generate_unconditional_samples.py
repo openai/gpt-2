e99ee3748b6a41e9532538bfc3af17f0b64a5caf
Neil Shepperd <nshepperd@google.com>
2019-05-15 13:37:29 -0400

    Add top_p to interactive_conditional_samples.py and generate_unconditional_samples.py.

--- src/generate_unconditional_samples.py
+++ src/generate_unconditional_samples.py
@@ -16,6 +16,7 @@ def sample_model(
     length=None,
     temperature=1,
     top_k=0,
+    top_p=0.0
 ):
     """
     Run the sample_model
@@ -35,6 +36,8 @@ def sample_model(
      considered for each step (token), resulting in deterministic completions,
      while 40 means 40 words are considered at each step. 0 (default) is a
      special setting meaning no restrictions. 40 generally is a good value.
+    :top_p=0.0 : Float value controlling diversity. Implements nucleus sampling,
+     overriding top_k if set to a value > 0. A good setting is 0.9.
     """
     enc = encoder.get_encoder(model_name)
     hparams = model.default_hparams()
@@ -54,7 +57,7 @@ def sample_model(
             hparams=hparams, length=length,
             start_token=enc.encoder['<|endoftext|>'],
             batch_size=batch_size,
-            temperature=temperature, top_k=top_k
+            temperature=temperature, top_k=top_k, top_p=top_p
         )[:, 1:]
 
         saver = tf.train.Saver()
@@ -72,4 +75,3 @@ def sample_model(
 
 if __name__ == '__main__':
     fire.Fire(sample_model)
-
--- src/interactive_conditional_samples.py
+++ src/interactive_conditional_samples.py
@@ -16,6 +16,7 @@ def interact_model(
     length=None,
     temperature=1,
     top_k=0,
+    top_p=0.0
 ):
     """
     Interactively run the model
@@ -34,6 +35,8 @@ def interact_model(
      considered for each step (token), resulting in deterministic completions,
      while 40 means 40 words are considered at each step. 0 (default) is a
      special setting meaning no restrictions. 40 generally is a good value.
+    :top_p=0.0 : Float value controlling diversity. Implements nucleus sampling,
+     overriding top_k if set to a value > 0. A good setting is 0.9.
     """
     if batch_size is None:
         batch_size = 1
@@ -57,7 +60,7 @@ def interact_model(
             hparams=hparams, length=length,
             context=context,
             batch_size=batch_size,
-            temperature=temperature, top_k=top_k
+            temperature=temperature, top_k=top_k, top_p=top_p
         )
 
         saver = tf.train.Saver()
@@ -84,4 +87,3 @@ def interact_model(
 
 if __name__ == '__main__':
     fire.Fire(interact_model)
-
