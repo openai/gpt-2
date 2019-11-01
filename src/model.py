37976c232da5b84ecf143ae3a40500d15c1ed74f
Logan Lieou <logan.lieou@gmail.com>
2019-10-31 23:58:00 -0500

    I'm so sorry
    Saves a line of code, you use this once.

--- src/model.py
+++ src/model.py
@@ -1,9 +1,8 @@
 import numpy as np
 import tensorflow as tf
-from tensorflow.contrib.training import HParams
 
 def default_hparams():
-    return HParams(
+    return tf.contrib.training.HParams(
         n_vocab=0,
         n_ctx=1024,
         n_embd=768,
