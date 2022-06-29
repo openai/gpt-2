dc68bbd6f64f9f854641b00919883425759658e4
Noah Huppert <3323504+Noah-Huppert@users.noreply.github.com>
2022-06-29 02:13:05 -0400

    Made training data have usernames
    Also made the <|endoftext|>token not be on new lines and wrap samples on both sides: https://datascience.stackexchange.com/questions/66394/how-does-bert-and-gpt-2-encoding-deal-with-token-such-as-startoftext-s/66399#66399

--- scripts/combine-training-data.py
+++ scripts/combine-training-data.py
@@ -7,7 +7,7 @@ DISCORD_MESSAGES_DIR = os.path.join(PROG_DIR, "../discord-messages")
 TRAINING_DATA_DIR = os.path.join(PROG_DIR, "../training-data")
 TRAINING_DATA_FILE = os.path.join(TRAINING_DATA_DIR, "discord-messages.txt")
 
-TRAINING_DELIMITER = "\n<|endoftext|>\n"
+TRAINING_DELIMITER = "<|endoftext|>"
 
 DISCORD_MSG_FILES = glob.glob(f"{DISCORD_MESSAGES_DIR}/*.json")
 
@@ -15,14 +15,18 @@ if not os.path.isdir(TRAINING_DATA_DIR):
     os.makedirs(TRAINING_DATA_DIR)
 
 num_msgs = 0
-with open(TRAINING_DATA_FILE, "w") as training_data_f:
+with open(TRAINING_DATA_FILE, "w", encoding="utf8") as training_data_f:
     for discord_msg_file_path in DISCORD_MSG_FILES:
-        with open(discord_msg_file_path, "r") as discord_msg_f:
+        with open(discord_msg_file_path, "r", encoding="utf8") as discord_msg_f:
             discord_msg_json = json.load(discord_msg_f)
 
-            msgs = list(filter(lambda t: len(t) > 0, [ msg['content'] for msg in discord_msg_json['messages'] ]))
-            training_data_f.writelines(TRAINING_DELIMITER.join(msgs))
-            
+            msgs = [
+                "".join([ TRAINING_DELIMITER, msg['author']['name'], "#", msg['author']['discriminator'], ": ", msg['content'], TRAINING_DELIMITER ])
+                for msg in filter(lambda msg: len(msg['content']) > 0, discord_msg_json['messages'])
+            ]
+
+            training_data_f.writelines("\n".join(msgs))
+        
             num_msgs += len(msgs)
             print(f"Wrote {len(msgs)} from {discord_msg_file_path} to training file")
 
